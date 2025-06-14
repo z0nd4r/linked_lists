3b267fad669da95766aca3dafb9f79c5b2613dd6
zondar__ <kirito98730tkm@gmail.com>
2025-06-14 18:43:25 +0200

    Add singly linked lists file

---
+++ singly.py
@@ -0,0 +1,15 @@
+class LinkedList:
+    class __Node:
+        def __init__(self, value):
+            self.value = value
+            self.next = None
+
+        def __repr__(self):
+            return f'value = {self.value}'
+
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+
+
+ll = LinkedList()
