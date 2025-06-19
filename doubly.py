43968b8d6bdeb9b05cdebc842e1e613cf7db522c
zondar__ <kirito98730tkm@gmail.com>
2025-06-19 16:27:05 +0200

    Doubly: add method "add_first"

--- doubly.py
+++ doubly.py
@@ -11,3 +11,40 @@ class LinkedList:
     def __init__(self):
         self.__head = None
         self.__tail = None
+
+    def add_first(self, value):
+        new_node = self.__Node(value)
+        if self.__is_empty():
+            self.__initialize(new_node)
+        else:
+            new_node.next = self.__head
+            self.__head.prev = new_node
+            self.__head = new_node
+
+    def add_last(self):
+        pass
+
+    def remove_first(self):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def print(self):
+        pass
+
+    def __is_empty(self):
+        return self.__head is None
+
+    def __initialize(self, new_node):
+        self.__head = self.__tail = new_node
+
+
+ll = LinkedList()
+ll.add_first(1)
+ll.add_first(2)
+
+print('')
